b831daae3f218733723870967439ec7e0e040012
Nikolay Kovyrshin <nikolaykovyrshin@outlook.com>
2024-04-13 12:55:29 +0300

    TG bot test 5 currency forecast with CurrencyConverter library

--- main.py
+++ main.py
@@ -4,13 +4,71 @@ import webbrowser
 
 import requests
 import telebot
+from currency_converter import CurrencyConverter
 from telebot import types
 
+converter = CurrencyConverter()
 bot = telebot.TeleBot('6974044588:AAHE5FE7oDPob7pJ_49I4sWzhE8K-_5_SOc')
 
 login = ""
 password = ""
 API_KEY = '7454ddf76d882614c5424f640009a0ff'
+amount = 0
+
+
+@bot.message_handler(commands=['convert'])
+def convert_currency(message):
+    bot.send_message(message.chat.id, 'Введите сумму')
+    bot.register_next_step_handler(message, summa)
+
+
+def summa(message):
+    global amount
+    try:
+        amount = int(message.text.strip())
+    except ValueError:
+        bot.send_message(message.chat.id, 'Неверный формат. Введит число.')
+        bot.register_next_step_handler(message, summa)
+        return
+
+    if amount > 0:
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        btn1 = types.InlineKeyboardButton('USD/EUR', callback_data='usd/eur')
+        btn2 = types.InlineKeyboardButton('EUR/USD', callback_data='eur/usd')
+        btn3 = types.InlineKeyboardButton('USD/GBP', callback_data='usd/gbp')
+        btn4 = types.InlineKeyboardButton('Другое значение', callback_data='else')
+        markup.add(btn1, btn2, btn3, btn4)
+        bot.send_message(message.chat.id, 'Выберите пару валют', reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, 'Сумма должна быть больше нуля. Введит число.')
+        bot.register_next_step_handler(message, summa)
+        return
+
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_data_currency(callback):
+    if callback.data != 'else':
+        values = callback.data.upper().split('/')
+        res = converter.convert(amount, values[0], values[1])
+        bot.send_message(callback.message.chat.id,
+                         f'Результат: {round(res, 2)}\nВведите следующую сумму для конвертации')
+        bot.register_next_step_handler(callback.message, summa)
+    else:
+        bot.send_message(callback.message.chat.id, 'Введите пару значений через слэш')
+        bot.register_next_step_handler(callback.message, my_currency)
+
+
+def my_currency(message):
+    try:
+        values = message.text.upper().split('/')
+        res = converter.convert(amount, values[0], values[1])
+        bot.send_message(message.chat.id,
+                         f'Результат: {round(res, 2)}\nВведите следующую сумму для конвертации')
+        bot.register_next_step_handler(message, summa)
+    except Exception:
+        bot.send_message(message.chat.id,
+                         f'Введите  существующую пару. Попробуйте ещё раз ')
+        bot.register_next_step_handler(message, my_currency)
 
 
 @bot.message_handler(content_types=['photo'])
@@ -62,7 +120,12 @@ def main(message):
     bot.register_next_step_handler(message, user_name)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['temp_forecast'])
+def weather_forecast(message):
+    bot.send_message(message.chat.id, 'Введите название города')
+    bot.register_next_step_handler(message, get_weather)
+
+
 def get_weather(message):
     city = message.text.strip().lower()
     result = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric')
