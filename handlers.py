03412e9185c4586b1f3470ea11f4696e853e7821
Nikolay Kovyrshin <nikolaykovyrshin@outlook.com>
2024-05-03 11:43:01 +0300

    delete category parser

--- handlers.py
+++ handlers.py
@@ -1,14 +1,13 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-
-import kb
+from utils import handle_json
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def start_handler(msg: Message):
-    await msg.answer("Выберите категорию для поиска", reply_markup=await kb.get_keyboard_text("https://www.avito.ru/"))
-    # data = handle_json("items.json")
-    # await msg.answer(data)
+    await msg.answer("Начинаю поиск квартир")
+    data = handle_json("items.json")
+    await msg.answer(data)
